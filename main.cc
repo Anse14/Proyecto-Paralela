55e352d5837e90f304bff44d112226e4cc28fc03
Anxxy <assm14401@gmail.com>
2021-12-14 18:19:52 -0500

    Updated

--- main.cc
+++ main.cc
@@ -1,15 +1,20 @@
 #include "./RngStream.h"
 #include <iostream>
+#include <chrono>
 #include <omp.h>
 using namespace std;
 
 auto main(int argc, const char** argv) -> int {
-  int nP = 200;
-  omp_set_num_threads(nP); // set number of threads
+  int nP = (argc > 1)? atoi(argv[1]) : 200;
+
+  omp_set_num_threads(1000); // set number of threads
+
   unsigned long seed[6] = {1806547166, 3311292359, 643431772,
                            1162448557, 3335719306, 4161054083};
   RngStream::SetPackageSeed(seed);
   RngStream RngArray[nP]; // array of RngStream objects
+
+  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
 #pragma omp parallel for
   for (int i = 0; i < nP; i++) {
@@ -17,6 +22,12 @@ auto main(int argc, const char** argv) -> int {
 #pragma omp critical
     { std ::cout << number << " "; }
   }
+
+  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
+  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
 
   return 0;
 }
