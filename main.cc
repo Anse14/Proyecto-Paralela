f5d5b87be9cc7dc1421f32fa1d1d8ef70d312c42
Anxxy <assm14401@gmail.com>
2021-12-14 12:26:23 -0500

    Updated

--- main.cc
+++ main.cc
@@ -4,51 +4,28 @@
 using namespace std;
 
 auto main() -> int {
-  // unsigned long seed[6] = {327612383, 317095578,  14704821,
-  // 884064067, 1017894425, 16401881};
-  // RngStream::SetPackageSeed(seed);
+  // int myRank;
+  //#pragma omp parallel private(myRank)
+  //{
+  // myRank = omp_get_thread_num();
+  //#pragma omp critical
+  //{ std ::cout << RngArray[myRank].RandU01() << " "; }
+  //}
+  // std ::cout << std ::endl;
 
-  // RngStream R;
-
-  // R.IncreasedPresic(true);
-  // R.SetAntithetic(true);
-
-  // cout << "Valor: " << R.RandU01() << endl;
-  // cout << "Valor: " << R.RandU01() << endl;
-  // cout << "Valor: " << R.RandU01() << endl;
-
-  // R.ResetNextSubstream();
-  //  R.AdvanceState(10, 6);
-  // cout << endl;
-
-  // cout << "Valor: " << R.RandU01() << endl;
-  // cout << "Valor: " << R.RandU01() << endl;
-  // cout << "Valor: " << R.RandU01() << endl;
-
-  // cout << endl;
-
-  // cout << R.RandInt(10, 20) << endl;
-  // cout << R.RandInt(10, 20) << endl;
-  // cout << R.RandInt(10, 20) << endl;
-  // cout << R.RandInt(10, 20) << endl;
-  // cout << R.RandInt(10, 20) << endl;
-
-  // R.WriteState();
-
-  //int nP = omp_get_num_procs();
-  omp_set_num_threads(2000); // set number of threads
+  int nP = 200;
+  omp_set_num_threads(nP); // set number of threads
   unsigned long seed[6] = {1806547166, 3311292359, 643431772,
                            1162448557, 3335719306, 4161054083};
   RngStream::SetPackageSeed(seed);
-  RngStream RngArray[2000]; // array of RngStream objects
-  int myRank;
-#pragma omp parallel private(myRank)
-  {
-    myRank = omp_get_thread_num();
+  RngStream RngArray[nP]; // array of RngStream objects
+
+#pragma omp parallel for
+  for (int i = 0; i < nP; i++) {
+    double number = RngArray[i].RandU01();
 #pragma omp critical
-    { std ::cout << RngArray[myRank].RandU01() << " "; }
+    { std ::cout << number << " "; }
   }
-  std ::cout << std ::endl;
 
   return 0;
 }
